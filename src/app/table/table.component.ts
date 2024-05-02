41c35551ecd592bf680b6348e2fbfc246798efc7
jinwoo <jinwoo7@x1.attlocal.net>
2024-05-02 08:37:11 -0700

    minor cleanup

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -19,7 +19,6 @@ export class TableComponent implements AfterViewInit {
 
   constructor(private vehicleService: VehicleService) {}
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  // displayedColumns = ['id', 'name'];
   displayedColumns = [
     'nickname',
     'type',
@@ -40,48 +39,3 @@ export class TableComponent implements AfterViewInit {
     console.log(this.table);
   }
 }
-
-// import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-// import { MatTable } from '@angular/material/table';
-// import { MatPaginator } from '@angular/material/paginator';
-// import { MatSort } from '@angular/material/sort';
-// import { TableDataSource, TableItem } from './table-datasource';
-// import { Vehicle } from '../models/vehicle.model';
-// import { VehicleService } from '../services/vehicle.service';
-
-// @Component({
-//   selector: 'app-table',
-//   templateUrl: './table.component.html',
-//   styleUrl: './table.component.css',
-// })
-// export class TableComponent implements AfterViewInit, OnInit {
-//   @ViewChild(MatPaginator) paginator!: MatPaginator;
-//   @ViewChild(MatSort) sort!: MatSort;
-//   @ViewChild(MatTable) table!: MatTable<Vehicle>;
-//   dataSource: TableDataSource;
-
-//   constructor(private vehicleService: VehicleService) {};
-
-//   ngOnInit(): void {
-//     this.dataSource = new TableDataSource(this.vehicleService);
-//   }
-
-//   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-//   displayedColumns = [
-//     'nickname',
-//     'assetType',
-//     'purchaseAmount',
-//     'appraisalAmount',
-//     'purchaseDate',
-//     'year',
-//     'make',
-//     'model',
-//     'trim',
-//   ];
-
-//   ngAfterViewInit(): void {
-//     this.dataSource.sort = this.sort;
-//     this.dataSource.paginator = this.paginator;
-//     this.table.dataSource = this.dataSource;
-//   }
-// }
